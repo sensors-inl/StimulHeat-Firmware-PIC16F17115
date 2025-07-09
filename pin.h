1332532851986ad675aa50acfc8f3e0e53f03b43
Matthieu Mesnage <matthieu.mesnage@insa-lyon.fr>
2025-07-09 14:21:24 +0000

    fix : add GPL-3 Licence

--- pin.h
+++ pin.h
@@ -1,6 +1,14 @@
-/* 
- * File: pin.h   
- * Author: MESNAGE Matthieu
+/*
+ * Project: StimulHeat
+ * Authors: Matthieu Mesnage, Bertrand Massot
+ * Company : INSA Lyon - INL
+ * Date: 2025-05-28
+ *
+ * This file is licensed under the GNU General Public License v3.0.
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * For commercial use or licensing inquiries, contact:
+ * bertrand.massot@insa-lyon.fr
  */
 
 // This is a guard condition so that contents of this file are not included more than once. 
