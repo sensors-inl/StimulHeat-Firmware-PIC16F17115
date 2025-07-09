3ce8bf7ceac20b310548c59fdc176e4a95792c98
Matthieu Mesnage <matthieu.mesnage@insa-lyon.fr>
2025-07-09 14:19:33 +0000

    fix : add GPL-3 Licence

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 /*
- * File:   main.c
- * Author: MESNAGE Matthieu
+ * Project: StimulHeat
+ * Authors: Matthieu Mesnage, Bertrand Massot
+ * Company : INSA Lyon - INL
+ * Date: 2025-05-28
+ *
+ * This file is licensed under the GNU General Public License v3.0.
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * For commercial use or licensing inquiries, contact:
+ * bertrand.massot@insa-lyon.fr
  */
-
+ 
 #include "xc.h"
 #include "pin.h"
 #include "i2c.h"
